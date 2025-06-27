804c669547bae1fa4f1525d178e1f393aaf3f1fa
Litesh <liteshsharma9466@gmail.com>
2025-06-28 00:13:26 +0530

    update the js files for booking

--- Static/js/find_parking.js
+++ Static/js/find_parking.js
@@ -20,9 +20,53 @@ function toggleBookSpot(){
 function handleVehicleChange(){
     vehicle_select=document.querySelector('#vehicle')
     selected_option=vehicle_select.options[vehicle_select.selectedIndex];
-    is_ev=selected_option.getAttribute('data-is-ev')==1
+    is_ev=selected_option.getAttribute('data-is-ev')=='True'
+    parking_options=document.querySelector('#parking-options')
+    
+    
+    if (is_ev){
+        parking_options.style.display='block'
+        document.getElementById('regular').checked=true
+        
+        
+    }else{
+        parking_options.style.display='none'
+        
+    }
+    updateRates()
+    
+}
+function updateRates(){
+    duration=document.querySelector('#duration')
+    selected_duration=duration.options[duration.selectedIndex]
+    time = parseInt(selected_duration.getAttribute('value'), 10)
+
+    ev_charging_rate=document.querySelector('#ev-charging-rate')
+    ev_rate=parseFloat(ev_charging_rate.getAttribute('data-ev-rate'))
+    regular_rate_id=document.querySelector('#regular-rate')
+    regular_rate=parseFloat(regular_rate_id.getAttribute('data-rate'))
+    selected_parking_type=document.querySelector('input[name="parking_type"]:checked')
+
+    estimate_cost=document.querySelector('#estimate_cost')
+
+    if (selected_parking_type && selected_parking_type.value ==='ev_charging'){
+        ev_charging_rate.style.display='block'
+        console.log(time*ev_rate + time*regular_rate);
+        estimate_cost.innerText=`Estimate Cost ₹ ${time*ev_rate + time*regular_rate}`
+        
+    }else{
+        ev_charging_rate.style.display='none'
+        console.log(time*regular_rate);
+        estimate_cost.innerText=`Estimate Cost ₹ ${time*regular_rate}`
+        
+    }
+        
 }
 
+function Duration(){
+
+    
+}
 
 document.addEventListener('DOMContentLoaded',()=>{
     toggleBookSpot()
