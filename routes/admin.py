2ad5d90a5495f473de4bfd48a4b2f472352bf853
Litesh <liteshsharma9466@gmail.com>
2025-07-03 01:20:44 +0530

    updated the css and html of admin_dashboard

--- routes/admin.py
+++ routes/admin.py
@@ -1,6 +1,6 @@
 from flask import Blueprint,render_template,request,redirect,url_for,session
 from routes.decorators import login_required,admin_required
-from models import ParkingLot
+from models import ParkingLot,User,ParkingSpot,Booking
 
 admin_bp=Blueprint('admin',__name__)
 
@@ -10,10 +10,25 @@ admin_bp=Blueprint('admin',__name__)
 def admin_dashboard():
     lots=ParkingLot.query.all()
     lot_id=request.args.get('lot_id')
+    user = User.query.get(session.get('user_id'))
+    spots=ParkingSpot.query.all()
+    
+    def spots_count(is_ev):
+        return ParkingSpot.query.filter_by(is_ev_spot=is_ev).count()
+    
+    total_regular_available_spots=ParkingSpot.query.filter_by(status='available',is_ev_spot=0).count()
+    total_ev_available_spots=ParkingSpot.query.filter_by(status='available',is_ev_spot=1).count()
+    total_regular_spots=spots_count(0)
+    total_ev_spots=spots_count(1)
+    
+    
+    active_bookings=Booking.query.count()
+    
+    total_users=User.query.count() - 1
     lot = None
     if lot_id:
         lot=ParkingLot.query.filter_by(id=lot_id).first()
     
-    return render_template('admin_dashboard.html',lots=lots,lot=lot)
+    return render_template('admin_dashboard.html',lots=lots,lot=lot,user=user,spots=spots,total_regular_available_spots=total_regular_available_spots,total_users=total_users,total_ev_available_spots=total_ev_available_spots,total_regular_spots=total_regular_spots,total_ev_spots=total_ev_spots,active_bookings=active_bookings)
 
 
