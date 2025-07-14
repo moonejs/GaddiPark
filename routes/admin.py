a77c36aadd898ac5c1d1e7ef5728aa1e85b75ea7
Litesh <liteshsharma9466@gmail.com>
2025-07-14 22:40:26 +0530

    added admin charts page

--- routes/admin.py
+++ routes/admin.py
@@ -50,3 +50,46 @@ def users_summery():
     spots=ParkingSpot.query.all()
     lots=ParkingLot.query.all()
     return render_template('users_summery.html',users=users,history=history,bookings=bookings,spots=spots,user=user,lots=lots)
+
+
+
+@admin_bp.route('/admin/charts')
+@login_required
+@admin_required
+def admin_charts():
+    user=User.query.get(session.get('user_id'))
+    regular_spots=History.query.filter_by(is_ev_spot=0).count()
+    ev_spots=History.query.filter_by(is_ev_spot=1).count()
+    history=History.query.all()
+    lots_dict={}
+    month_dict={}
+    for h in history:
+        lot=ParkingLot.query.get(h.lot_id)
+        lots_dict[lot.name]=lots_dict.get(lot.name,0)+1
+        month = h.exit_time.strftime('%m')
+        month_dict[month]=month_dict.get(month,0)+h.total_amount_paid
+    
+    month_names_chart3_labels= ['Jan', 'Feb', 'Mar', 'Apr', 'May','Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    
+    revenue=[0]*12
+    month_num=list(month_dict.keys())
+    
+    total_revenue_lst=list(month_dict.values())
+    
+    for i in range(len(month_num)):
+        revenue[int(month_num[i])]=total_revenue_lst[i]
+    
+    chart3_data=revenue
+    
+    
+    lot_names_chart2_labels=list(lots_dict.keys())
+    chart2_data=list(lots_dict.values())
+    
+    best_lot = lot_names_chart2_labels[chart2_data.index(max(chart2_data))]
+    
+    peak_month=month_names_chart3_labels[int(month_num[total_revenue_lst.index(max(total_revenue_lst))])-1]
+    
+    
+    chart1_labels=['Regular Vehicles','Ev Vehicles']
+    chart1_data=[regular_spots,ev_spots]
+    return render_template('admin_charts.html',chart1_labels=chart1_labels,chart1_data=chart1_data,lot_names_chart2_labels=lot_names_chart2_labels,chart2_data=chart2_data,month_names_chart3_labels=month_names_chart3_labels,chart3_data=chart3_data,user=user,best_lot=best_lot,peak_month=peak_month)
